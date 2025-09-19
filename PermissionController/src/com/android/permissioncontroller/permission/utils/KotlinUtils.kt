eb32d6903fc1c07d7eab3137eb097d4c2fd48ccc
Nate Myren <ntmyren@google.com>
2025-09-19 10:51:05 -0700

    Revert "Revert "Display the system gallery storage permission as..."
    Revert submission 35132783-galleryRevert-24
    
    Reason for revert: relanding
    
    Reverted changes: /q/submissionid:35132783-galleryRevert-24
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:30d2cddebb4c5452c7622ad01f781f70f6ebf426
    Merged-In: Ic9734367a5e3fdbae1f895ba6f9fd57e9171f4c3
    Change-Id: Ic9734367a5e3fdbae1f895ba6f9fd57e9171f4c3

--- PermissionController/src/com/android/permissioncontroller/permission/utils/KotlinUtils.kt
+++ PermissionController/src/com/android/permissioncontroller/permission/utils/KotlinUtils.kt
@@ -782,7 +782,8 @@ object KotlinUtils {
             group.permGroupInfo,
             newPerms,
             group.hasInstallToRuntimeSplit,
-            group.specialLocationGrant
+            group.specialLocationGrant,
+            group.specialFixedStorageGrant,
         )
     }
 
@@ -902,7 +903,8 @@ object KotlinUtils {
                 group.permGroupInfo,
                 newPerms,
                 group.hasInstallToRuntimeSplit,
-                group.specialLocationGrant
+                group.specialLocationGrant,
+                group.specialFixedStorageGrant,
             )
         // If any permission in the group is one time granted, start one time permission session.
         if (newGroup.permissions.any { it.value.isOneTime && it.value.isGranted }) {
@@ -1184,7 +1186,8 @@ object KotlinUtils {
                 group.permGroupInfo,
                 newPerms,
                 group.hasInstallToRuntimeSplit,
-                group.specialLocationGrant
+                group.specialLocationGrant,
+                group.specialFixedStorageGrant,
             )
 
         if (wasOneTime && !anyPermsOfPackageOneTimeGranted(app, newGroup.packageInfo)) {
